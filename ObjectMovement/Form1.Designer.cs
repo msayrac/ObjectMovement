f4cb222c285af3563af777a810d2b0d8600aaad6
msayrac <msayracode@gmail.com>
2024-02-03 14:06:17 +0300

    Add project files.

---
+++ ObjectMovement/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ObjectMovement
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.panel6 = new System.Windows.Forms.Panel();
+			this.panel8 = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.timer2 = new System.Windows.Forms.Timer(this.components);
+			this.timer3 = new System.Windows.Forms.Timer(this.components);
+			this.timer4 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.White;
+			this.panel1.Location = new System.Drawing.Point(33, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(27, 647);
+			this.panel1.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.White;
+			this.panel2.Location = new System.Drawing.Point(245, 125);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(30, 404);
+			this.panel2.TabIndex = 1;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.White;
+			this.panel3.Location = new System.Drawing.Point(245, 102);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(628, 26);
+			this.panel3.TabIndex = 2;
+			// 
+			// panel4
+			// 
+			this.panel4.BackColor = System.Drawing.Color.White;
+			this.panel4.Location = new System.Drawing.Point(1142, -2);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(27, 647);
+			this.panel4.TabIndex = 1;
+			// 
+			// panel5
+			// 
+			this.panel5.BackColor = System.Drawing.Color.White;
+			this.panel5.Location = new System.Drawing.Point(869, 102);
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new System.Drawing.Size(31, 411);
+			this.panel5.TabIndex = 3;
+			// 
+			// panel6
+			// 
+			this.panel6.BackColor = System.Drawing.Color.White;
+			this.panel6.Location = new System.Drawing.Point(272, 503);
+			this.panel6.Name = "panel6";
+			this.panel6.Size = new System.Drawing.Size(628, 26);
+			this.panel6.TabIndex = 3;
+			// 
+			// panel8
+			// 
+			this.panel8.BackColor = System.Drawing.Color.White;
+			this.panel8.Location = new System.Drawing.Point(59, 617);
+			this.panel8.Name = "panel8";
+			this.panel8.Size = new System.Drawing.Size(1085, 28);
+			this.panel8.TabIndex = 6;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(66, 514);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(173, 97);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 7;
+			this.pictureBox1.TabStop = false;
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 10;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// timer2
+			// 
+			this.timer2.Interval = 10;
+			this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+			// 
+			// timer3
+			// 
+			this.timer3.Interval = 10;
+			this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+			// 
+			// timer4
+			// 
+			this.timer4.Interval = 10;
+			this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Gray;
+			this.ClientSize = new System.Drawing.Size(1168, 645);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.panel8);
+			this.Controls.Add(this.panel6);
+			this.Controls.Add(this.panel5);
+			this.Controls.Add(this.panel4);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Panel panel5;
+		private System.Windows.Forms.Panel panel6;
+		private System.Windows.Forms.Panel panel8;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Timer timer2;
+		private System.Windows.Forms.Timer timer3;
+		private System.Windows.Forms.Timer timer4;
+	}
+}
+
